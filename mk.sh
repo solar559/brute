64b43ac983f4233ce9137555d826586a7f7e4710
Eduard Bagdasaryan <dictiano@gmail.com>
2017-07-24 21:33:17 +0300

    Wall added

--- mk.sh
+++ mk.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-g++ -o brute main.cpp generator.cpp tests.cpp argparser.cpp brute.cpp -std=c++0x -pthread 
+g++ -Wall -o brute main.cpp generator.cpp tests.cpp argparser.cpp brute.cpp -std=c++0x -pthread 
