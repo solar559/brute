8e6bdf9c4f4cbf618b8666c448d1f88b1ede00ad
Eduard Bagdasaryan <dictiano@gmail.com>
2017-08-11 17:30:08 +0300

    added TestParam

--- mk.sh
+++ mk.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
 
+echo ================== Parameters ================
+echo $TestParam
+echo ==============================================
+
 g++ -Wall -o brute main.cpp generator.cpp tests.cpp argparser.cpp brute.cpp -std=c++0x -pthread 
