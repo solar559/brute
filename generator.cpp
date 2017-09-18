06e1fe4661b0212b56973d058d95fde887dc1483
Eduard Bagdasaryan <dictiano@gmail.com>
2017-09-18 14:29:46 +0300

    test_branch8 change 2

--- generator.cpp
+++ generator.cpp
@@ -106,7 +106,7 @@ void StrGenerator::PrintIVec(IntVec const &vec)
 {
     for(int i = 0; i < vec.size(); ++i)
     {
-	cout << vec[i] << " " << endl;
+        cout << vec[i] << " " << endl;
     }
 }
 
