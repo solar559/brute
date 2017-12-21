9343e0456564c1275930efa74e183a31e1940246
Eduard Bagdasaryan <dictiano@gmail.com>
2017-12-21 14:05:52 +0300

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -35,8 +35,7 @@ int main(int argc, char**argv)
     helper.PrintResult();
     std::chrono::duration<double> elapsed_seconds = end-start;
     
-    std::cout << "elapsed time: " << elapsed_seconds.count() << endl;
-    
+    std::cout << "elapsed time: " << elapsed_seconds.count() << endl;    
     return 0;
 }
 
