d26e73054f23bce023413016f0dd51ee796309a7
Eduard Bagdasaryan <dictiano@gmail.com>
2018-01-11 17:50:58 +0300

    Update tst.bat

--- tst.bat
+++ tst.bat
@@ -5,4 +5,3 @@ rem Evaluating execution time for one thread
 brute.exe 2 3 65-90:97-122 5c8df559fbbe5156c06996be9c34397f 1
 rem Evaluating execution time for four threads
 brute.exe 2 3 65-90:97-122 5c8df559fbbe5156c06996be9c34397f 4
-
